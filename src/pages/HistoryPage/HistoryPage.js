1c9b59c59aede643e9c8985a9edc5a9180712262
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-10-22 18:10:29 -0300

    Adição da biblioteca react-calendar, modificações no layout do calendário e requisição do histórico de hábitos diário

--- src/pages/HistoryPage/HistoryPage.js
+++ src/pages/HistoryPage/HistoryPage.js
@@ -3,9 +3,29 @@ import TopBar from "../../components/TopBar";
 import { useAuth } from "../../providers/auth";
 import LoadingPage from "../../assets/styles/LoadingPage";
 import Menu from "../../components/Menu";
+import Calendar from "react-calendar";
+import { useEffect } from "react";
+import axios from "axios";
+import URL from "../../constants/url";
+/* import 'react-calendar/dist/Calendar.css'; */
 
 function HistoryPage() {
-    const { userLogin } = useAuth();
+  const { userLogin } = useAuth();
+
+  useEffect(() => {
+    if (userLogin !== undefined) {
+      axios
+        .get(`${URL}/habits/history/daily`, {
+          headers: {
+            Authorization: `Bearer ${userLogin.token}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    }
+  }, [userLogin]);
 
   if (userLogin === undefined) {
     return (
@@ -21,11 +41,14 @@ function HistoryPage() {
       <Heading>
         <Title>Histórico</Title>
       </Heading>
-      <Report>
+      {/* <Report>
         <p>
           Em breve você poderá ver o histórico dos seus hábitos aqui!
         </p>
-      </Report>
+      </Report> */}
+      <CalendarContainer>
+        <Calendar />
+      </CalendarContainer>
       <Menu />
     </PageContainer>
   );
@@ -59,11 +82,133 @@ const Title = styled.h2`
   margin-bottom: 5px;
 `;
 
-const Report = styled.div`
+/* const Report = styled.div`
   margin-left: 17px;
   margin-top: 17px;
   font-family: "Lexend Deca", sans-serif;
   font-weight: 400;
   font-size: 18px;
   color: #666666;
+`; */
+
+const CalendarContainer = styled.div`
+  margin-left: 19px;
+  margin-top: 11px;
+  .react-calendar {
+    width: 335px;
+    max-width: 100%;
+    background: white;
+    border-radius: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.5em;
+  }
+  .react-calendar--doubleView {
+    width: 700px;
+  }
+  .react-calendar--doubleView .react-calendar__viewContainer {
+    display: flex;
+    margin: -0.5em;
+  }
+  .react-calendar--doubleView .react-calendar__viewContainer > * {
+    width: 50%;
+    margin: 0.5em;
+  }
+  .react-calendar,
+  .react-calendar *,
+  .react-calendar *:before,
+  .react-calendar *:after {
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+  .react-calendar button {
+    margin: 0;
+    border: 0;
+    outline: none;
+  }
+  .react-calendar button:enabled:hover {
+    cursor: pointer;
+  }
+  .react-calendar__navigation {
+    display: flex;
+    height: 44px;
+    margin-bottom: 1em;
+  }
+  .react-calendar__navigation button {
+    min-width: 44px;
+    background: none;
+  }
+  .react-calendar__navigation button:disabled {
+    background-color: #f0f0f0;
+  }
+  .react-calendar__navigation button:enabled:hover,
+  .react-calendar__navigation button:enabled:focus {
+    background-color: #e6e6e6;
+  }
+  .react-calendar__month-view__weekdays {
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 0.75em;
+  }
+  .react-calendar__month-view__weekdays__weekday {
+    padding: 0.5em;
+  }
+  .react-calendar__month-view__weekNumbers .react-calendar__tile {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 0.75em;
+    font-weight: bold;
+  }
+  .react-calendar__month-view__days__day--weekend {
+    color: #d10000;
+  }
+  .react-calendar__month-view__days__day--neighboringMonth {
+    color: #757575;
+  }
+  .react-calendar__year-view .react-calendar__tile,
+  .react-calendar__decade-view .react-calendar__tile,
+  .react-calendar__century-view .react-calendar__tile {
+    padding: 2em 0.5em;
+  }
+  .react-calendar__tile {
+    max-width: 100%;
+    padding: 10px 6.6667px;
+    background: none;
+    text-align: center;
+    line-height: 30px;
+  }
+  .react-calendar__tile:disabled {
+    background-color: #f0f0f0;
+  }
+  .react-calendar__tile:enabled:hover,
+  .react-calendar__tile:enabled:focus {
+    background-color: #e6e6e6;
+  }
+  .react-calendar__tile--now {
+    background: #ffff76;
+  }
+  .react-calendar__tile--now:enabled:hover,
+  .react-calendar__tile--now:enabled:focus {
+    background: #ffffa9;
+  }
+  .react-calendar__tile--hasActive {
+    background: #76baff;
+  }
+  .react-calendar__tile--hasActive:enabled:hover,
+  .react-calendar__tile--hasActive:enabled:focus {
+    background: #a9d4ff;
+  }
+  .react-calendar__tile--active {
+    background: #006edc;
+    color: white;
+  }
+  .react-calendar__tile--active:enabled:hover,
+  .react-calendar__tile--active:enabled:focus {
+    background: #1087ff;
+  }
+  .react-calendar--selectRange .react-calendar__tile--hover {
+    background-color: #e6e6e6;
+  }
 `;
